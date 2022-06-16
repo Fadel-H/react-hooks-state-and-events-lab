c9a51ef979450cbfc13c94593cc07f89cdea5d59
Fadel <fadel.g.heji@gmail.com>
2022-06-16 00:01:16 -0400

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState("")
+
+  function handleInCart(){
+    setInCart((inCart) => inCart === "" ? "in-cart" : "")
+  }
+
+  const color = inCart ==="" ? "yellow" : "purple"
   return (
-    <li className="">
+    <li className= {inCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" style={{background:color}} onClick={handleInCart}>{inCart === "" ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
